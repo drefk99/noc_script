236b6320a70ebd0507d422872fcde87017710508
drefk99 <drefk94@gmail.com>
2017-02-28 15:55:28 -0600

    Update script.sh

--- script_perl/script.sh
+++ script_perl/script.sh
@@ -9,54 +9,104 @@ fin=`cat datf`
 
 #CPU
 firefox "http://192.168.50.13/cacti/graph_xport.php?local_graph_id=31&rra_id=0&graph_start=$inicio&graph_end=$fin"
+mv 'palomar - CPU Usage.csv' "'palomar - CPU Usage'.csv"
+
 #Nu0
 firefox "http://192.168.50.13/cacti/graph_xport.php?local_graph_id=32&rra_id=0&graph_start=$inicio&graph_end=$fin"
+mv 'palomar - Traffic - StackSub-St1-2.csv' "'palomar - Traffic - Nu0'.csv"
+
 #vl1
 firefox "http://192.168.50.13/cacti/graph_xport.php?local_graph_id=6&rra_id=0&graph_start=$inicio&graph_end=$fin"
+mv 'palomar - Traffic - Vl1.csv' "'palomar - Traffic - Vl1'.csv"
+
 #A12
 firefox "http://192.168.50.13/cacti/graph_xport.php?local_graph_id=15&rra_id=0&graph_start=$inicio&graph_end=$fin"
+mv 'palomar - Traffic - Gi1_0_10.csv' "'_A12_ - Traffic - Gi1_0_10'.csv"
+
 #A1
 firefox "http://192.168.50.13/cacti/graph_xport.php?local_graph_id=7&rra_id=0&graph_start=$inicio&graph_end=$fin"
+mv 'palomar - Traffic - Gi1_0_1.csv' "'_A1_ - Traffic - Gi1_0_1'.csv"
+
 #A2
 firefox "http://192.168.50.13/cacti/graph_xport.php?local_graph_id=8&rra_id=0&graph_start=$inicio&graph_end=$fin"
+mv 'palomar - Traffic - Gi1_0_2.csv' "'_A2_ - Traffic - Gi1_0_2'.csv"
+
 #A3
 firefox "http://192.168.50.13/cacti/graph_xport.php?local_graph_id=9&rra_id=0&graph_start=$inicio&graph_end=$fin"
+mv 'palomar - Traffic - Gi1_0_3.csv' "'_A3_ - Traffic - Gi1_0_3'.csv"
+
 #A4
 firefox "http://192.168.50.13/cacti/graph_xport.php?local_graph_id=10&rra_id=0&graph_start=$inicio&graph_end=$fin"
+mv 'palomar - Traffic - Gi1_0_4.csv' "'_A4_ - Traffic - Gi1_0_4'.csv"
+
 #A421
 firefox "http://192.168.50.13/cacti/graph_xport.php?local_graph_id=30&rra_id=0&graph_start=$inicio&graph_end=$fin"
+mv 'palomar - Traffic - Gi1_0_5.csv' "'_A421_ - Traffic - Gi1_0_5'.csv"
+
 #A6
 firefox "http://192.168.50.13/cacti/graph_xport.php?local_graph_id=11&rra_id=0&graph_start=$inicio&graph_end=$fin"
+
+
 #A7
 firefox "http://192.168.50.13/cacti/graph_xport.php?local_graph_id=12&rra_id=0&graph_start=$inicio&graph_end=$fin"
+
+
 #A8
 firefox "http://192.168.50.13/cacti/graph_xport.php?local_graph_id=14&rra_id=0&graph_start=$inicio&graph_end=$fin"
+
+
 #Almacen
 firefox "http://192.168.50.13/cacti/graph_xport.php?local_graph_id=17&rra_id=0&graph_start=$inicio&graph_end=$fin"
+
+
 #Apoyo a la Docencia
 firefox "http://192.168.50.13/cacti/graph_xport.php?local_graph_id=26&rra_id=0&graph_start=$inicio&graph_end=$fin"
+
+
 #Cajas 
 firefox "http://192.168.50.13/cacti/graph_xport.php?local_graph_id=18&rra_id=0&graph_start=$inicio&graph_end=$fin"
+
+
 #CCA
 firefox "http://192.168.50.13/cacti/graph_xport.php?local_graph_id=25&rra_id=0&graph_start=$inicio&graph_end=$fin"
+
+
 #CETED
 firefox "http://192.168.50.13/cacti/graph_xport.php?local_graph_id=28&rra_id=0&graph_start=$inicio&graph_end=$fin"
+
+
 #Core1
 firefox "http://192.168.50.13/cacti/graph_xport.php?local_graph_id=29&rra_id=0&graph_start=$inicio&graph_end=$fin"
+
+
 #Gobierno
 firefox "http://192.168.50.13/cacti/graph_xport.php?local_graph_id=27&rra_id=0&graph_start=$inicio&graph_end=$fin"
+
+
 #Idiomas 
 firefox "http://192.168.50.13/cacti/graph_xport.php?local_graph_id=16&rra_id=0&graph_start=$inicio&graph_end=$fin"
+
+
 #Libreria
 firefox "http://192.168.50.13/cacti/graph_xport.php?local_graph_id=24&rra_id=0&graph_start=$inicio&graph_end=$fin"
+
+
 #Posgrado1
 firefox "http://192.168.50.13/cacti/graph_xport.php?local_graph_id=21&rra_id=0&graph_start=$inicio&graph_end=$fin"
+
+
 #Posgrado2
 firefox "http://192.168.50.13/cacti/graph_xport.php?local_graph_id=22&rra_id=0&graph_start=$inicio&graph_end=$fin"
+
+
 #Seminarios
 firefox "http://192.168.50.13/cacti/graph_xport.php?local_graph_id=19&rra_id=0&graph_start=$inicio&graph_end=$fin"
+
+
 #UIM
 firefox "http://192.168.50.13/cacti/graph_xport.php?local_graph_id=20&rra_id=0&graph_start=$inicio&graph_end=$fin"
+
+
 
 datemon=$(date +%b)
 
