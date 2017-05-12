729a4d22e042577ba17330b03749b8fb8dd3684c
drefk99 <drefk94@gmail.com>
2017-05-12 17:00:00 -0500

    no special changes

--- script_R/noc_analize.R
+++ script_R/noc_analize.R
@@ -4,3 +4,5 @@ INhisto<-read.csv(paste(args[1],"'INhistorico'.csv", sep="/"),sep = ',', header 
 OUThisto<-read.csv(paste(args[1],"'OUThistorico'.csv", sep="/"),sep = ',', header = TRUE, skip = 0)
 
 fit<-kmeans(data.pru[,2:31],3)
+trapru<-t(data.pru)
+plot(trapru[2:29,1:1],col=fit$cluster)
