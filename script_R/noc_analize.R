54bc2ecb8dbc23e0538c4f82f7cc060f03aebfd6
drefk99 <drefk94@gmail.com>
2017-05-29 17:59:18 -0500

    Checking wich is the best way to do clusters

--- script_R/noc_analize.R
+++ script_R/noc_analize.R
@@ -5,6 +5,12 @@ INhisto<-read.csv(paste(args[1],"'INhistorico'.csv", sep="/"),sep = ',', header 
 
 OUThisto<-read.csv(paste(args[1],"'OUThistorico'.csv", sep="/"),sep = ',', header = TRUE, skip = 0)
 
-fit<-kmeans(data.pru[,2:31],3)
+
 trapru<-t(data.pru)
+fit<-kmeans(trapru[2:29,],3)
 plot(trapru[2:29,1:1],col=fit$cluster)
+#res<-NbClust(trapru[1:28,2:10],distance="euclidean", min.nc=2, max.nc=6, method="kmeans")
+mat.pru<-matrix(trapru,nrow = 28, ncol = 820)
+
+
+#res<-NbClust(as.matrix(trapru[2:29,]),distance="euclidean", min.nc=2, max.nc=6, method="kmeans")
